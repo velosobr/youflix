655d69f705e26a0c3e1e5648aa625d59cea01de6
lino.veloso <lino.veloso@dextra-sw.com>
2020-06-04 00:35:48 -0300

    workday 04/06 - Ajuste do LocalGroupVideosGroupListStorageImpl.kt, que não estava empilhando os videos, mas sim substituindo.
    Signed-off-by: lino.veloso <lino.veloso@dextra-sw.com>

--- app/src/main/java/com/cursoandroid/youflix/navigationBar/videosScreen/repository/service/VideosListAccess.kt
+++ app/src/main/java/com/cursoandroid/youflix/navigationBar/videosScreen/repository/service/VideosListAccess.kt
@@ -18,6 +18,9 @@ interface VideosListAccess {
     &channelId=UCVHFbqXqoYvEWM1Ddxl0QDg
     &q=desenvolvimento+android
 
+    https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=YOUR_API_KEY
+    &fields=items(id,snippet(channelId,title,categoryId),statistics)&part=snippet,statistics
+
     https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&maxResults=20&key=LocalData&channelId=UCVHFbqXqoYvEWM1Ddxl0QDg
      */
     @GET("search")
